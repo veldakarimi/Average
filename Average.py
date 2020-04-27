a5817d856c9a63590a5ab99a2c8d5e96b92acbe4
Velda Kiara <veldakarimi@gmail.com>
2020-04-28 01:08:54 +0300

    average of numbers

---
+++ Average.py
@@ -0,0 +1,9 @@
+# Write your average function here:
+def average(num1, num2):
+  average = (num1 + num2) / 2
+  return average
+
+print(average(1, 100))
+# The average of 1 and 100 is 50.5
+print(average(1, -1))
+# The average of 1 and -1 is 0
